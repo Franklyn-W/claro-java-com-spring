c30a34aaa8175800b137a989fb1027d5248122d9
Franklyn Winston <franklyn_winston@hotmail.com>
2024-09-18 22:02:53 -0300

    Mapeamente das entidades

---
+++ src/main/java/me/dio/domain/model/Account.java
@@ -0,0 +1,69 @@
+package me.dio.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true) // indica que a coluna number da tabela tb_account é do tipo unique, não devendo aceitar repetição
+    private String number;
+
+    private String agency;
+    
+    @Column(scale = 13, precision = 2) // define que a coluna terá 2 casas decimais e um total de 13 numeros (11 interiros e 2 decimais)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", scale = 13, precision = 2) // alterado nome da coluna, pois a palavra limit, pode ser uma palavra reservada
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+}
---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,83 @@
+package me.dio.domain.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity(name = "tb_user") // Indica que a claase representa uma tabela (opcional nesse caso: informar o nome da tabela)
+public class User {
+
+    @Id // Identifica a variavel id, como o id da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Indica que o valor deve ser gerado automaticante e se trata de um identificador
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL) // Indica o tipo de relacionamento entre as tabelas e o comportamento entre elas.
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+
+}
